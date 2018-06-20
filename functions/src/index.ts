b70a7baddeac65857977dc7ae1a51fd953c48625
Matt Yao <mattyao1984@gmail.com>
2018-06-20 15:54:10 +1000

    Added piggybank-server code and ready to deploy

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,48 @@
+import 'zone.js/dist/zone-node';
+import 'reflect-metadata';
+import { enableProdMode } from '@angular/core';
+// Express Engine
+import { ngExpressEngine } from '@nguniversal/express-engine';
+// Import module map for lazy loading
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+
+import * as express from 'express';
+import { join } from 'path';
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Faster server renders w/ Prod mode (dev mode never needed)
+enableProdMode();
+
+// Express server
+const app = express();
+
+const DIST_FOLDER = join(process.cwd(), 'dist');
+
+// * NOTE :: leave this as require() since this file is built Dynamically from webpack
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('../dist/piggybank-server/main');
+
+// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
+app.engine('html', ngExpressEngine({
+    bootstrap: AppServerModuleNgFactory,
+    providers: [
+        provideModuleMap(LAZY_MODULE_MAP)
+    ]
+}));
+
+app.set('view engine', 'html');
+app.set('views', join(DIST_FOLDER, 'piggybank'));
+
+// Example Express Rest API endpoints
+// app.get('/api/**', (req, res) => { });
+
+// Server static files from /browser
+app.get('*.*', express.static(join(DIST_FOLDER, 'piggybank'), {
+    maxAge: '1y'
+}));
+
+// All regular routes use the Universal engine
+app.get('*', (req, res) => {
+    res.render('index', { req });
+});
+
+export const ssr = functions.https.onRequest(app); // refer this to firebase.json => rewrites function field
